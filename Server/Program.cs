c094e14678025eb53e61cf8bb999a2896ddc3fc1
Fentan-Kyc <di-ma02@mail.ru>
2023-11-15 19:57:47 +0300

    Initial

---
+++ Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 80);
+            listener.Start();
+
+            var client = listener.AcceptTcpClient();
+            using(var stream = client.GetStream())
+            {
+                using(var reader = new StreamReader(stream))
+                {
+                    while (true)
+                    {
+                        var result = reader.ReadLine();
+                        Console.WriteLine(result);
+                    }
+                }
+            }
+        }
+    }
+}
