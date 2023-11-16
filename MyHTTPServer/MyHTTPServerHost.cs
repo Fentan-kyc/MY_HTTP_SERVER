731e4939e410c166c3992f55e094cd253e505210
Fentan-kyc <di-ma02@mail.ru>
2023-11-16 16:00:00 +0300

    New lib

---
+++ MyHTTPServer/MyHTTPServerHost.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MyHTTPServer
+{
+    public sealed class MyHTTPServerHost
+    {
+        private readonly int _port;
+
+        public MyHTTPServerHost(int port = 80)
+        {
+            _port = port;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Hello");
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 80);
+            listener.Start();
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                using (var stream = client.GetStream())
+                {
+                    using (var reader = new StreamReader(stream))
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        for (string line = null; line != string.Empty; line = reader.ReadLine())
+                        {
+                            var result = reader.ReadLine() ?? string.Empty;
+                            Console.WriteLine(result);
+                        }
+
+                        writer.WriteLine("Hi, It's the simplest server in the hood.");
+                    }
+                }
+            }
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyHTTPServer;
+using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -9,29 +10,8 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-
-            TcpListener listener = new TcpListener(IPAddress.Any, 80);
-            listener.Start();
-
-            while(true)
-            {
-            var client = listener.AcceptTcpClient();
-            using(var stream = client.GetStream())
-            {
-                using(var reader = new StreamReader(stream))
-                using(var writer = new StreamWriter(stream))
-                {
-                    for(string line = null; line != string.Empty; line = reader.ReadLine())
-                    {
-                        var result = reader.ReadLine();
-                        Console.WriteLine(result);
-                    }
-
-                    writer.WriteLine("Hi, It's the simplest server in the hood.");
-                }
-            }
-            }
+            MyHTTPServerHost host = new MyHTTPServerHost();
+            host.Start();
         }
     }
 }
